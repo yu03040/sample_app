b3d3cdeb92cd7ec466f590490b635183147d5e35
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-09-27 13:39:44 +0900

    Add : hello アクションを追加
    リスト 3.4、リスト 3.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # application_controller.rb の hello アクションを呼び出す
+  root "application#hello"
 end
