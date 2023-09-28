d7bc8842967bd7e329ef43806b9c20a3605eb4bb
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-09-29 02:21:55 +0900

    Fix : コメントを修正
    (cherry picked from commit 7923792b23545d4606d1ddf5d3c6e5b15fead223)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # 指定したパスに GET メソッドでアクセスがあった場合
+  # 表示するページを指定する
   get 'static_pages/home'
   get 'static_pages/help'
-  # application_controller.rb の hello アクションを呼び出す
+  # ルートドメインにアクセスしたとき
+  # 表示するページを指定する
   root "application#hello"
 end
