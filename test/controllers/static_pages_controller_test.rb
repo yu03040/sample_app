74cb9c11c4ed2281923c6ff4b3a990e4ff56836e
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-09-29 15:47:31 +0900

    Fix : StaticPages コントローラのテストには、いくつか繰り返しがあったので修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,35 @@
+# テストを実行するために必要なライブラリや設定を含む test_helper.rb ファイルを読み込み
 require "test_helper"
 
+# ActionDispatch::IntegrationTestは
+# 統合テストの基本クラスであり、 HTTP リクエストをシミュレートしてコントローラーの動作をテストする
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+
+  # 各テストメソッドが実行される前に実行されるメソッド
+  # テスト内で使用する共通の変数や設定を初期化
+  def setup
+    # インスタンス変数
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  # home アクションが正しく動作することを確認
+  test "should get home" do
+    get static_pages_home_url # StaticPagesController の home アクションを呼び出し、その結果をテスト
+    assert_response :success # ページが正常にロードされたことを確認
+    assert_select "title", "Home | #{@base_title}" # ページのタイトルが正しいことを確認
+  end
+
+  # help アクションが正しく動作することを確認
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
+  # about アクションが正しく動作することを確認
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
 end
