4639d46649d8d911d83c33d34379766d94cba10e
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-04 15:38:53 +0900

    add ; contact ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # 指定したパスに GET メソッドでアクセスがあった場合
   # 表示するページを指定する
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   # ルートドメインにアクセスしたとき
   # 表示するページを指定する
   root "application#hello"
