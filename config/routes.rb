9d82d511cf6b8e231048a3fe9172166ad03bbf50
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-04 16:07:35 +0900

    Fix : rootルーティングに対するテストを修正
    リスト 3.43, 3,44
    
    (cherry picked from commit 065fa673ee5d8a3b39142bd548931cdb283ddd32)

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get "static_pages/contact"
   # ルートドメインにアクセスしたとき
   # 表示するページを指定する
-  root "application#hello"
+  root "static_pages#home"
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,31 +12,32 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   # home アクションが正しく動作することを確認
   test "should get home" do
     get static_pages_home_url # StaticPagesController の home アクションを呼び出し、その結果をテスト
     assert_response :success # ページが正常にロードされたことを確認
-    assert_select "title", "Home | #{@base_title}" # ページのタイトルが正しいことを確認
   end
 
   # help アクションが正しく動作することを確認
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
   end
 
   # about アクションが正しく動作することを確認
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
   end
 
   # contact アクションが正しく動作することを確認
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
   end
 end
