749e51e0f4752d806dc4a6c0b0982d12f5502774
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-11 10:55:03 +0900

    Fix : ページタイトルの定義の修正
    # 4.1.2 カスタムヘルパー
    
    (cherry picked from commit 0ff0b2df2578b3610982d28d5889ffe2a6162fdb)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = "")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    # page_title が存在するか？
+    if page_title.empty?
+      base_title
+    else
+      # #{} は " " の中で変数内の文字列を取り出す（式展開）
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
