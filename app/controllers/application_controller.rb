42e9c5ed526f55ecaeb47a6cd4f7dbcc5213a5b2
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-21 14:16:53 +0900

    Add : application.html.erb に HTML 構造, home.html.erbビューに特別な要素をいくつか追加
    # 5.1.1
    
    (cherry picked from commit d91dd977f07c484befd6a1e7a7a0cdfdf8789e67)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# すべてのコントローラの基底クラス
 class ApplicationController < ActionController::Base
-    def hello
-        # 文字列をブラウザで読み込んで表示する
-        render html: "hello, world!"
-    end
+  def hello
+    # 文字列をブラウザで読み込んで表示する
+    render html: 'hello, world!'
+  end
 end
