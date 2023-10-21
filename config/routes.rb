d33b713277c5100274b79e260df5cdeefa3b7da4
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-21 15:48:21 +0900

    test : コンテンツページのテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # 指定したパスに GET メソッドでアクセスがあった場合
   # 表示するページを指定する
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # ルートドメインにアクセスしたとき
   # 表示するページを指定する
-  root "static_pages#home"
+  root 'static_pages#home'
+  get 'static_pages/contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -41,5 +41,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get contact' do
     get static_pages_contact_url
     assert_response :success
+    assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
   end
 end
