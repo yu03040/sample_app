cc507c9ac5d01050ff9a11ae52c2cfaba4bbd58d
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-25 13:59:50 +0900

    Add : レイアウトのリンクに対するテストを追加
    # 5.3..4

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# レイアウトに関するテストを実行するために使用されるクラス
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'layout links' do
+    get root_path
+    assert_template 'static_pages/home' # 特定のテンプレートを使用していることを確認
+    assert_select 'a[href=?]', root_path, count: 2 # HTML内で root_path へのリンクが2つ存在するかどうかを検証する
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+  end
+end
