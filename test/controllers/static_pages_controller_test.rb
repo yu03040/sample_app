fdd7c9ac49c2a9514e58ee05f049e1adf84bf32d
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-25 13:16:36 +0900

    Fix : StaticPagesで扱う新しい名前付きルーティングに対するテストの修正
    # 5.3.2
    
    (cherry picked from commit 1c6ef51a41f5ed2544b6c3a94f0daaba7de4dc55)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,40 +6,28 @@ require 'test_helper'
 # ActionDispatch::IntegrationTestは
 # 統合テストの基本クラスであり、 HTTP リクエストをシミュレートしてコントローラーの動作をテストする
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # 各テストメソッドが実行される前に実行されるメソッド
-  # テスト内で使用する共通の変数や設定を初期化
-  def setup
-    # インスタンス変数
-    @base_title = 'Ruby on Rails Tutorial Sample App'
-  end
-
-  test 'should get root' do
-    get root_url
-    assert_response :success
-  end
-
   # home アクションが正しく動作することを確認
   test 'should get home' do
-    get static_pages_home_url # StaticPagesController の home アクションを呼び出し、その結果をテスト
+    get root_path # StaticPagesController の home アクションを呼び出し、その結果をテスト
     assert_response :success # ページが正常にロードされたことを確認
     assert_select 'title', 'Ruby on Rails Tutorial Sample App' # ページのタイトルが指定されたテキストであることを確認
   end
 
   # help アクションが正しく動作することを確認
   test 'should get help' do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
   # about アクションが正しく動作することを確認
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
   # contact アクションが正しく動作することを確認
   test 'should get contact' do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
   end
