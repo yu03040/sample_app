7340d990d8d652c062286bc825b8aa372c3de12d
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-10-25 13:15:25 +0900

    Fix :静的なページのルーティングを修正
    # 5.3.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
+# frozen_string_literal: true
+
+# ウェブアプリケーションの URL 構造とアクションのマッピングを定義する
 Rails.application.routes.draw do
-  # 指定したパスに GET メソッドでアクセスがあった場合
-  # 表示するページを指定する
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  # ルートドメインにアクセスしたとき
-  # 表示するページを指定する
+  # ルートドメインにアクセスしたとき、表示するページを指定する
   root 'static_pages#home'
-  get 'static_pages/contact'
+  # 指定したパスに GET メソッドでアクセスがあった場合、各アクションを指定する
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
