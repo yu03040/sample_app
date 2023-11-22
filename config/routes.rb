0c8b0e1e5e6dc0a0e47bca927bdfdb47c1db4f69
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-11-22 12:10:32 +0900

    Add : Users コントローラーの作成
    演習含む
    
    # 5.4.1

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@
 
 # ウェブアプリケーションの URL 構造とアクションのマッピングを定義する
 Rails.application.routes.draw do
+  get 'users/new'
   # ルートドメインにアクセスしたとき、表示するページを指定する
   root 'static_pages#home'
   # 指定したパスに GET メソッドでアクセスがあった場合、各アクションを指定する
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
