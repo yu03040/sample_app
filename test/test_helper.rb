1de3c1f015a8d88584c27ddb1d44d4c779c641e0
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-11-22 11:55:26 +0900

    Add : application_helper_test.rb の作成
    # 5.3.3
    
    (cherry picked from commit cadc85d0c1ac94f01703bee00693046a689a4909)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 # 環境変数 RAILS_ENV が設定されていない場合に、デフォルトでテスト環境を使用するように設定
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 
 # Rails アプリケーションの設定と環境を読み込み
 # このファイルは Rails アプリケーションの起動に必要であり、
 # テスト環境を正しくセットアップするために使用する
-require_relative "../config/environment"
+require_relative '../config/environment'
 
 # Rails のテストフレームワークで提供されるテストユーティリティを読み込み
 # テストの実行やテスト用データベースのセットアップなど、テストに必要な機能が利用できる
-require "rails/test_help"
+require 'rails/test_help'
 
 # Minitestのレポーターを読み込み
 # レポーターはテスト結果の表示方法をカスタマイズするために使用される
-require "minitest/reporters"
+require 'minitest/reporters'
 
 # Minitestのレポーターをアクティブにする
 # これにより、テスト結果がより読みやすく表示される
@@ -20,14 +22,17 @@ Minitest::Reporters.use!
 
 # テストケースの基本クラス
 # Railsのテストスイートのテストケースを定義するために使用される
-class ActiveSupport::TestCase
-  # 指定のワーカー数でテストを並列実行する
-  parallelize(workers: :number_of_processors)
+module ActiveSupport
+  class TestCase
+    # 指定のワーカー数でテストを並列実行する
+    parallelize(workers: :number_of_processors)
 
-  # test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
-  # fixture はテストデータベースに事前にセットアップされたデータで
-  # テストケースで使用される
-  fixtures :all
+    # test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
+    # fixture はテストデータベースに事前にセットアップされたデータで
+    # テストケースで使用される
+    fixtures :all
+    include ApplicationHelper
 
-  # （すべてのテストで使うその他のヘルパーメソッドは省略）
+    # （すべてのテストで使うその他のヘルパーメソッドは省略）
+  end
 end
