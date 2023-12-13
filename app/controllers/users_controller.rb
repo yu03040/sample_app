8399febde3fa506575b971a71553418373ba3f32
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-12-13 13:04:52 +0900

    Add : ユーザー登録の失敗に対応できるcreateアクションを追加
    正しいフォーム
    
    # 7.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user]) # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
