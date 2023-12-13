ba6653f5e5b7417bfd8ddb463a9578aa9cd22909
yu03040 <131416689+yu03040@users.noreply.github.com>
2023-12-13 13:57:28 +0900

    Fix : createアクションでStrong Parametersを使うように修正
    # 7.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user]) # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
     else
       render 'new', status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
